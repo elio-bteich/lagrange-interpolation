d1f255070b1faa179c093026d1ea5c7c0415fd89
Elio Bteich <eliobteich115@gmail.com>
2023-02-12 19:44:09 +0100

    adjusted main config

--- main.cpp
+++ main.cpp
@@ -11,20 +11,20 @@
 #include "lagrange.hpp"
 
 #define BOUND_A 0.0
-#define BOUND_B 1.0
+#define BOUND_B 2.0
 
 using namespace std;
 
 /**
  *
- * @param x le point pour lequel on veut évaluer la fonction f1
+ * @param x le point pour lequel on veut évaluer la fonction func
  *
- * @returns l'image de x par la fonction f1
+ * @returns l'image de x par la fonction func
  *
  * fonction qui retourne x/sqrt(x)
  *
  */
-double f1(double x)
+double func(double x)
 {
     return x / sqrt(x);
 }
@@ -32,16 +32,16 @@ double f1(double x)
 int main(int argc, char **argv)
 {
 
-    const uint64_t INTERPOLATION_X_NB = 100; // nombre de points d'interpolation
+    const uint64_t INTERPOLATION_X_NB = 10; // nombre de points d'interpolation
     const uint64_t X_NB = 10000;          // nombre de points d'echantillonage de la courbe
 
     double *tab_x = xvals(BOUND_A, BOUND_B, INTERPOLATION_X_NB - 1);
 
-    double *tab_y = yvals(&f1, tab_x, INTERPOLATION_X_NB);
+    double *tab_y = yvals(&func, tab_x, INTERPOLATION_X_NB);
 
     cout << setprecision(16) << "Lagrange de x/sqrt(x) en x=0.55 est " << approx_lagrange(tab_x, tab_y, INTERPOLATION_X_NB, 0.55) << endl;
 
-    cout << "f(0.55) = " << f1(0.55) << endl;
+    cout << "f(0.55) = " << func(0.55) << endl;
 
     double *x_echant = new double[X_NB];
 
@@ -52,13 +52,13 @@ int main(int argc, char **argv)
         x_echant[i] = BOUND_A + i * distAB / X_NB;
     }
 
-    //double *y_echant = yvals(&f1, x_echant, X_NB);
+    //double *y_echant = yvals(&func, x_echant, X_NB);
     double *y_echant = new double[X_NB];
     for (int i = 0; i < X_NB; i++) {
         y_echant[i] = approx_lagrange(tab_x, tab_y, INTERPOLATION_X_NB, x_echant[i]);
     }
 
-    double *y_func = yvals(&f1, x_echant, X_NB);
+    double *y_func = yvals(&func, x_echant, X_NB);
 
     plot(x_echant, y_func, X_NB, "courbe de x/sqrt(x)", "plotFunc");
 
@@ -71,7 +71,7 @@ int main(int argc, char **argv)
 
     double *errorRel = (double *)calloc(X_NB, sizeof(double));
 
-    estimateRelError(errorRel, tab_x, tab_y, INTERPOLATION_X_NB, &f1, BOUND_A, BOUND_B, X_NB);
+    estimateRelError(errorRel, tab_x, tab_y, INTERPOLATION_X_NB, &func, BOUND_A, BOUND_B, X_NB);
 
     double maxRelError = getMax(errorRel, X_NB);
     cout << " max relative error : " << maxRelError << endl;
@@ -83,7 +83,7 @@ int main(int argc, char **argv)
     plot(x_echant, errorRel, X_NB, const_cast<char*>(char_description), "plotRelError");
 
     double *errorAbs = (double *)calloc(X_NB, sizeof(double));
-    estimateAbsError(errorAbs, tab_x, tab_y, INTERPOLATION_X_NB, &f1, BOUND_A, BOUND_B, X_NB);
+    estimateAbsError(errorAbs, tab_x, tab_y, INTERPOLATION_X_NB, &func, BOUND_A, BOUND_B, X_NB);
     double maxAbsError = getMax(errorAbs, X_NB);
     cout << " max absolute error : " << maxAbsError << endl;
 
